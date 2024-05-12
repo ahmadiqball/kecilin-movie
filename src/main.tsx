9ccfa14b541e1df314aee05bcfb6e93db66b4507
Ahmad Iqbal <ahmadiqbalshinichi@gmail.com>
2024-05-12 16:15:25 +0800

    chore: setting up react query that will be used for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,21 @@ import './index.css';
 import 'virtual:uno.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/route';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      cacheTime: 1000 * 60 * 60 * 24,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
