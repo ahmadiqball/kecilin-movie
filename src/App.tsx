a1595febbe8d18cb956f8a26e57d4aaaf9ba84bd
Ahmad Iqbal <ahmadiqbalshinichi@gmail.com>
2024-05-12 16:10:41 +0800

    chore: setting up unocss for utility class library

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../vite.svg';
 import './App.css';
 
 function App() {
@@ -13,13 +12,7 @@ function App() {
           href='https://vitejs.dev'
           target='_blank'
           rel='noreferrer'
-        >
-          <img
-            src={viteLogo}
-            className='logo'
-            alt='Vite logo'
-          />
-        </a>
+        ></a>
         <a
           href='https://react.dev'
           target='_blank'
